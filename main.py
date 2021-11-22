75c1710e48133997ec90365ba1fc615b504bd8d3
Yaffles <89337312+Yaffles@users.noreply.github.com>
2021-11-22 21:06:26 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from typingGame import main
-import curses
 from curses import wrapper
-from time import time
-import json
 
 
 def main2(stdscr):
+    """Gives option to start different programs"""
     main(stdscr)
 
 
